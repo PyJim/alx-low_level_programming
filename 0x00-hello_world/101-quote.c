b92014b8339da1f431a835bab07756d90c1359fc
Jimmy Kodwo Essel <jimmyessel@outlook.com>
2022-09-08 22:24:20 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <errno.h>
+#include <unistd.h>
 
 /**
- * main - Prints a statement
- *
- * Description: A C program that prints a statement to the standard error.
- *
- * Return: return 0
+ * main - Prints exactly "and that piece of art is useful"
+ * - Dora Korpar , 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19n\n", stderr);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19n\n", 59);
 	return (1);
 }
