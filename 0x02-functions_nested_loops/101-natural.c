d977eb0104fc8de2dc27307524e2a2400e2b5e76
Jimmy Kodwo Essel <jimmyessel@outlook.com>
2022-09-14 03:36:56 +0000

    nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -19,9 +19,10 @@ for (i = 0; i < 1024; ++i)
 if ((i % 3) == 0)
 {
 sum3 = sum3 + i;
-} 
+}
 else if ((i % 5) == 0)
-{																					sum5 = sum5 + i;
+{
+sum5 = sum5 + i;
 }
 }
 sum = sum3 + sum5;
