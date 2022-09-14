67f7f9d29a7a33e65dda78402be9c06603ddf76d
Jimmy Kodwo Essel <jimmyessel@outlook.com>
2022-09-14 03:06:55 +0000

    nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -18,10 +18,14 @@ for (i = 1; i <= 50; ++i)
 if (j != 20365011074)
 {
 printf("%ld, ", j);
-} else															{
+}
+else							
+{
 printf("%ld\n", j);
-}															next = j + k;
-j = k;															k = next;
+}
+next = j + k;
+j = k;
+k = next;
 }
 return (0);
 }
