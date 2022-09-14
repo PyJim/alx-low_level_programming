ad948ac8781d786ee173539409642a8b1de72730
Jimmy Kodwo Essel <jimmyessel@outlook.com>
2022-09-14 03:34:30 +0000

    nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - computes and prints the sum of all the multiples
- *   * of 3 or 5 below 1024
- *    * Return: Always 0 (Success)
- *     */
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
@@ -18,7 +18,8 @@ for (i = 0; i < 1024; ++i)
 {
 if ((i % 3) == 0)
 {
-sum3 = sum3 + i;																			} 
+sum3 = sum3 + i;
+} 
 else if ((i % 5) == 0)
 {																					sum5 = sum5 + i;
 }
