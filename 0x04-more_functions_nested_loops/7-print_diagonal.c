5b110151612d7f6b32b26f812924c6426b8a8964
Jimmy Kodwo Essel <jimmyessel@outlook.com>
2022-09-17 00:07:52 +0000

    nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal
+ * @n: parameter
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+if (n <= 0)
+_putchar('\n');
+else
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+if (i != 0)
+																					{																					int s;
+for (s = 1; s <= i; s++)
+_putchar(' ');
+																					_putchar('\\');
+_putchar('\n');
+}
+}
+}
